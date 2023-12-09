26d695791762c62ad0159e684971c0ca8075f7f3
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-12-10 00:20:05 +0700

    3.20 phonebook database step8

--- models/person.js
+++ models/person.js
@@ -21,7 +21,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{7,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "Person phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
